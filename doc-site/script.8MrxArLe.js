f58c519fed95b16b104a66d0f843b9d3e4ce73f2
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2025-02-18 20:50:48 -0500

    remove doc-site

--- doc-site/script.8MrxArLe.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.querySelector('.mobile-nav .hamburger');
-    const mobileNav = document.querySelector('.sidebar');
-
-    menuButton.addEventListener('click', function() {
-        mobileNav.classList.toggle('open');
-    });
-});
